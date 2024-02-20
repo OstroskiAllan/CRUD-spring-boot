03a1cc89403eb917d958d90c07df68d5222f2a3e
Ostroski <allan_s11@hotmail.com>
2024-02-20 17:10:58 -0300

    Att futura de possivel front no projeto

--- demo/src/main/java/borth/crud/demo/controller/HomeController.java
+++ demo/src/main/java/borth/crud/demo/controller/HomeController.java
@@ -1,5 +1,4 @@
 package borth.crud.demo.controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- demo/src/main/resources/static/script.js
+++ demo/src/main/resources/static/script.js
@@ -1,47 +1,22 @@
-const formulario = document.getElementById('formulario');
-const nome = document.getElementById('nome');
-const id = document.getElementById('id');
-const listaUsuario = document.getElementById('listaUsuario');
-
-
-
-// Event listener para o formulário
-formulario.addEventListener('submit', event => {
+// Função para criar novo usuário
+function createUser(userData) {
     event.preventDefault();
-    const id = id.value;
-    const nome = nome.value;
-    const method = id ? 'PUT' : 'POST';
-    const url = id ? `/usuarios/${id}` : '/usuarios';
-    const body = JSON.stringify({ nome: nome });
-    fetch(url, {
-        method,
+    fetch('/usuario/save', {
+        method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body
+        body: JSON.stringify(userData),
     })
-    .then(response => response.json())
-    .then(() => {
-        id.value = '';
-        nome.value = '';
-        loadUserList();
-    });
-});
-
-function loadUserList() {
-    fetch('/usuarios')
-        .then(response => response.json())
-        .then(usuarios => {
-            const listaUsuario = document.getElementById('listaUsuario');
-            listaUsuario.innerHTML = '';
-            usuarios.forEach(usuario => {
-                listaUsuario.innerHTML += `<p>${usuario.nome}</p>`;
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao carregar lista de usuários:', error);
-        });
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao criar usuário');
+        }
+        console.log('Novo usuário criado com sucesso');
+    })
+    .catch(error => console.error('Erro ao criar usuário:', error));
 }
 
-// Chamar a função para carregar a lista de usuários ao carregar a página
-loadUserList();
+
+
+
